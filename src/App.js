99f4739d442de69748b770ef43d04f51e0967502
LTo28 <to.larry92@gmail.com>
2019-06-27 21:11:00 -0700

    removed imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header/Header'
 import Game from './components/Game/Game'
-import Footer from './components/Footer/Footer'
 
 function App() {
   return (
